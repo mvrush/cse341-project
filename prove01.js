98ced3e605506b3328954367a5fe69ae6faf5988
mvrush <mvrush@gmail.com>
2021-09-18 18:11:12 -0600

    Prove01 Complete

--- prove01.js
+++ prove01.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const routes = require('./prove01-routes'); // since this is not a global module we add hte local path to routes.js with ./ and you can also drop the .js from routes.
+const routes = require('./prove01-routes'); // since this is not a global module we add the local path to routes.js with ./ and you can also drop the .js from routes.
 
 console.log(routes.someText);
 
