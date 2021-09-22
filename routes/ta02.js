e33c9ceb718c8c838bef8bec238a598359967c10
mvrush <mvrush@gmail.com>
2021-09-21 20:40:46 -0600

    update routes

--- routes/ta02.js
+++ routes/ta02.js
@@ -2,13 +2,16 @@
 // Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
+const nameList = ['Peter', 'James', 'John'];
 
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    //added during team
+    nameArray: nameList
   });
 });
 
