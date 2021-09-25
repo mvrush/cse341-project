b4a56e7dff0ad47b9be7f2de121c4be4e9717d93
mvrush <mvrush@gmail.com>
2021-09-25 14:06:50 -0600

    prove02 Complete

--- prove02/app.js
+++ prove02/app.js
@@ -1,43 +1,32 @@
-/************************
-  This is the completed app.js we finished for the 'Working with Dynamic Content & Adding Templating Engines' module
- *************************/
-// The following lines basically write your own server. Console.log is what causes it to log down there in the terminal window.
+const path = require('path');
 
-const path = require('path'); // Helps us construct a path that will work on all operating systems.
+const express = require('express');
 
-const express = require('express'); // this imports Express.js so we can use express. 
-
-const bodyParser = require('body-parser'); // this constant stores the 'body-parser' function that we added usng 'npm install --save body-parser' . It's used to extract user entered data from the body.
+const bodyParser = require('body-parser');
  
-const app = express(); // this constant which you can name whatever you want, stores the express() function. That is how we use express. This app is a valid request handler.
+const app = express()
 
-app.set('view engine', 'ejs'); // 'app.set' sets a global config value. In this case it's saying that the dynamic view engine is 'ejs'.
-app.set('views', 'views'); // this 'app.set' sets the dynamic views folder. The first 'views' means look for views in the second value, 'views', which is the folder. You can use a folder named something other than 'views'. Also, if you leave the second value out, the default is to a folder called 'views'.
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminData = require('./routes/admin'); // this is the absolute path to our admin.js file in the routes folder.
+const adminData = require('./routes/admin');
 
-const shopRoutes = require('./routes/shop'); // this is the absolute path to our shop.js file in the routes folder.
+const shopRoutes = require('./routes/shop');
 
-// 'use' allows us to add a new middleware function (middleware is like a code plugin). You have to use the 'next' value at the end of your use() function to move past it's section on to the next function.
-// or you need to send a response 'res' if you don't need to move the 'next' middleware. The '/' is the path for the function. If you want it to go to a different page you would type '/pagename' .
-// the order that you put these functions is very important because they're always read top to bottom.
 
-app.use(bodyParser.urlencoded({extended: false})); // uses the 'bodyParser' const to use the 'body-parser' function. The line marks it as deprecated for some reason. This has to be placed before the page routes.
+app.use(bodyParser.urlencoded({extended: false}));
 
-// The following line allows us to serve static files from our 'public' folder. It's called 'static' and it ships with express. It ultimately gives the 'public' folder read only access.
-// we basically call 'static' from 'express' then we use 'path.join' to show a path to our 'public' folder. '__dirname' is the root folder of our project and then it finds the 'public' folder in there.
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// again, order matters. So list 'shopRoutes' second because it is calling our '/' page and that '/' exists in all addresses.
-app.use('/admin', adminData.routes); // uses the 'adminData' constant as an object, not a function, to use our admin.js file in the routes folder. 'routes' is actually defined in the admin.js file. I added '/admin' to this statement so that it will attach /admin to the path of the pages in admin.js, this is how we filter routes.
 
-app.use(shopRoutes); // uses the 'shopRoutes constant as an object to use our shop.js file in the routes folder.
+app.use('/admin', adminData.routes);
 
-// The following line handles everything that our code and routes aren't programmed to do. It's kind of like a catch-all line. We use 'app.use' to handle all http methods, not just GET or POST.
-// We can use '/' in this line but that's the default anyway so we don't need to use it.
-// NOTE: We can chain another method before 'send', in this case the 'status' method to set the 404 status code. You can use 'status' before any 'send' and it will show up in the Console or Network view in Dev Tools.
+app.use(shopRoutes);
+
+
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: '404 Page Not Found', path: '' }); // This is what EJS uses to render a view. It passes in a value for 'pageTitle' which we can then call with <%= pageTitle %> in our html template.
+    res.status(404).render('404', { pageTitle: '404 Page Not Found', path: '' });
 });
 
-app.listen(3000); // this uses Express (through the app const) to start the server and listen to port 3000. It basically creates our server using Express.
+app.listen(3000);
--- prove02/routes/admin.js
+++ prove02/routes/admin.js
@@ -1,42 +1,36 @@
-// This is the route that handles the creation of products which the admin of our shop can do.
+const path = require('path');
 
-const path = require('path'); // require('path') is the 'path' core module in Express. We assign it to the constant names 'path'. It allows us to specify file paths and will even translate them for the users os whether that be Linux or Windows.
+const express = require('express');
 
-const express = require('express'); // Again we require express to be used on this page.
+const rootDir = require('../util/path');
 
-const rootDir = require('../util/path'); // The constant 'rootDir' is enabled by the 'path.js' file found in the 'util' folder. We use it to specify our root directory.
+const router = express.Router();
 
-const router = express.Router(); // We create the router constant to hold the 'express.Router()' function. Router() is an Express function that routes things.
+const products = [];
 
-const products = []; // This will be our products array. The const is the array itself which means we can add and delete elements inside the array.
 
-// you always have to have the more specific middleware (i.e. '/add-product') before the less specific middleware (i.e. '/') because '/' will be true on all path requests.
-// since we defined the 'router' const above, we now use 'router.use' instead of 'app.use'. Also the same arguments can be used such as 'use', 'post', 'get' or others.
-// since we only want to use 'get' requests in this first one we use 'router.get'.
-// The implied path for the following line is '/admin/add-product => GET' because we added the /admin path on the app.js page.
 router.get('/add-product', (req, res, next) => {
-    // res.render tells router.get to render our 'add-product.ejs' page. Then it gives it a JavaScript object filled with keynames that have data assigned to them.
     res.render('add-product', {
         pageTitle: 'Add Product',
-        path: '/admin/add-product', // this is the path the user types in the browser address
-        formsCSS: true, // uses the CSS for forms
-        productCSS: true, // uses the CSS for product
-        activeAddProduct: true // when on this page the link in nav shows as active
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
     });
 });
 
-// we use 'app.post' instead of 'app.use' because 'app.post' will only parse post requests. 'app.use' will parse both 'get' and 'post' requests. You can also use 'app.get' to parse only 'get' requests.
-// The implied path for the following line is '/admin/add-product => POST' because we added the /admin path on the app.js page.
+
 router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title, price: req.body.price, desc: req.body.desc}); // this will push a new object into the array. It puts the 'title' into the array and it pulls it from the 'reqested' 'body' from the 'title' field of said 'body'.
-    console.log(products); // lets me see in the console what is in the array.
-    res.redirect('/'); // this redirects the user to '/' after extracting their input. redirect() is a built-in express function.
+    products.push({title: req.body.title, price: req.body.price, desc: req.body.desc});
+    console.log(products);
+    res.redirect('/');
 });
 
 router.post('/remove-product', (req, res, next)=>{
     console.log("Removing "+req.body.title);
     const index = products.indexOf(req.body.title);
-    if (index > -1) {
+    console.log(index);
+    if (index > -2) {
       products.splice(index, 1);
       message = "";
     }
