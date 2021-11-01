03770e31f35d2c4ec680fa0a0ff3bda82d00e0c5
mvrush <mvrush@gmail.com>
2021-11-01 15:12:06 -0600

    Prove08 Split into MVC

--- controllers/pa08.js
+++ controllers/pa08.js
@@ -1,1 +1,64 @@
 /************* Prove08 Controller **************/
+const fileName ='https://byui-cse.github.io/cse341-course/lesson03/items.json';
+
+const https = require('https');
+const { isUndefined } = require('util');
+
+let contents='';
+https.get(fileName,(res) => {
+    let body = "";
+
+    res.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    res.on("end", () => {
+        try {
+            contents = JSON.parse(body);
+        } catch (error) {
+            console.error(error.message);
+        };
+    });
+}).on("error", (error) => {
+    console.error(error.message);
+});
+
+exports.getBody = (req, res, next) => {
+    console.log(req.query.search);
+    res.render('pages/pa08', {
+      title: 'Prove Assignment 08',
+      path: '/pa08', // For pug, EJS
+      json: contents,
+      filterMesh: contents
+    });
+  };
+
+exports.postBody = (req, res, next) => {
+    filterJson=contents;
+    console.log(req.body);
+    if(req.body.search !== undefined)
+    {
+    
+      filterJson = contents.filter((item)=>{
+      mesh= req.body.search;
+      console.log(mesh);
+      if(item.name.includes(mesh)||item.description.includes(mesh))
+      {
+        return true;
+      }
+      for( tag of item.tags)
+      {
+        if(tag.includes(mesh))
+          return true;
+      }
+    });
+    }
+    res.render('pages/pa08', {
+      title: 'Prove Assignment 08',
+      path: '/pa08', // For pug, EJS
+      json: contents,
+      filterMesh: filterJson
+    });
+  };
+
+
--- routes/pa08.js
+++ routes/pa08.js
@@ -1,66 +1,9 @@
 const express = require('express');
-const fileName ='https://byui-cse.github.io/cse341-course/lesson03/items.json';
 const router = express.Router();
 
-const https = require('https');
-const { isUndefined } = require('util');
+const pa08Controller = require('../controllers/pa08');
 
-let contents='';
-https.get(fileName,(res) => {
-    let body = "";
+router.get('/', pa08Controller.getBody);
+router.post('/', pa08Controller.postBody);
 
-    res.on("data", (chunk) => {
-        body += chunk;
-    });
-
-    res.on("end", () => {
-        try {
-            contents = JSON.parse(body);
-        } catch (error) {
-            console.error(error.message);
-        };
-    });
-}).on("error", (error) => {
-    console.error(error.message);
-});
-
-
-router.get('/', (req, res, next) => {
-  console.log(req.query.search);
-  res.render('pages/pa08', {
-    title: 'Prove Assignment 08',
-    path: '/pa08', // For pug, EJS
-    json: contents,
-    filterMesh: contents
-  });
-});
-router.post('/', (req, res, next) => {
-  filterJson=contents;
-  console.log(req.body);
-  if(req.body.search !== undefined)
-  {
-  
-    filterJson = contents.filter((item)=>{
-    mesh= req.body.search;
-    console.log(mesh);
-    if(item.name.includes(mesh)||item.description.includes(mesh))
-    {
-      return true;
-    }
-    for( tag of item.tags)
-    {
-      if(tag.includes(mesh))
-        return true;
-    }
-  });
-  }
-  res.render('pages/pa08', {
-    title: 'Prove Assignment 08',
-    path: '/pa08', // For pug, EJS
-    json: contents,
-    filterMesh: filterJson
-  });
-});
-//filterMesh
 module.exports = router;
-//file.close;
